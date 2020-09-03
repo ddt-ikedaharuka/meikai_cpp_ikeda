a3960406bde3a7559a7d7125917ebe0a51be55ef
ikedaharuka <ddt.hikeda@gmail.com>
2020-09-03 19:19:18 +0900

    演習1-2 using指令を削除して、std::coutに変更

---
+++ meikai-e-1-2/meikai-e-1-2.cpp
@@ -0,0 +1,13 @@
+﻿/**
+List1-1のプログラムからusing指令を削除して、coutをstd::coutに変更したプログラムを作成せよ。
+作成者：池田
+作成日：2020/9/3
+*/
+#include <iostream>
+
+int main()
+{
+    std::cout << "初めてのC++プログラム。\n";
+    std::cout << "画面に出力しています。\n";
+
+}
