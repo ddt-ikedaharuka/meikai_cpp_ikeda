bdc07fda527156ec3cf1b69eebeeff1698fd965e
ikedaharuka <ddt.hikeda@gmail.com>
2020-09-14 18:52:04 +0900

    meikai-e-1-6~2-4ソースコード追加

---
+++ meikai-e-1-14.cpp
@@ -0,0 +1,24 @@
+﻿/**
+キーボードから読み込んだ整数値±5の範囲の整数値をランダムに生成して表示するプログラムを生成せよ。
+作成者：池田
+作成日：9/7
+*/
+#include<ctime>
+#include<cstdlib>
+#include <iostream>
+
+int main()
+{
+    int x;
+
+    std::cout << "整数値:";
+
+    std::cin >> x;
+
+    std::srand(time(NULL));
+
+    std::rand() % (x + 5) + (x - 5);
+
+    std::cout << "その値の±5の乱数を生成しました。それは" << x << "です。\n";
+}
+
---
+++ meikai-e-2-2.cpp
@@ -0,0 +1,27 @@
+﻿/**
+List2-5の最後のelseを、else if(n == 0)に変更するとどうなるかを検討せよ。
+作成者：池田
+作成日：9/11
+*/
+#include <iostream>
+
+int main()
+{
+	int n;
+
+	std::cout << "整数値：";
+
+	std::cin >> n;
+
+	if (n > 0) {
+		std::cout << "その値は正です。\n";
+
+		} else if (n < 0){
+			std::cout << "その値は負です。\n";
+		} else if (n == 0){
+			std::cout << "その値は0です。\n";
+		}
+
+
+}
+
